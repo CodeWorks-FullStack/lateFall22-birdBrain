47210e35800ee494591602d689dc2f9c30bdc1c5
SamanthaDison <samnicdis@gmail.com>
2022-12-01 15:03:56 -0700

    push

--- client/app/Models/Bird.js
+++ client/app/Models/Bird.js
@@ -44,7 +44,7 @@ export class Bird {
         </div>
         <div class="modal-footer">
           <button type="button" class="btn bird-btn" data-bs-dismiss="modal">Close</button>
-          <button type="button" class="btn bird-btn-green">Save changes</button>
+          <button type="button" class="btn bird-btn-green">Done Creepin'</button>
         </div>`
     }
 
@@ -55,28 +55,33 @@ export class Bird {
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
      
-        <form onsubmit="app.birdsController.peepBird()">
+        <form class="row" onsubmit="app.birdsController.peepBird()">
           <div class="modal-body">
-            <div class="form-floating mb-3">
+            <div class="form-floating mb-3 col-12">
               <input type="text" class="form-control" id="name" name="name" placeholder="Bird Name">
               <label for="name">Bird Name</label>
             </div>
-            <div class="form-floating mb-3">
+            <div class="form-floating mb-3 col-12">
               <input required type="url" class="form-control" id="img" name="img" placeholder="Bird Image">
               <label for="img">Bird Image</label>
             </div>
-          </div>
-          <input type="checkbox" id="canFly"  name="canFly">
-          <div>
-          </div>
+          <div class="col-12">
           <select name="size" class="form-select" id="size">
             <option value="small">Small</option>
             <option value="medium">Medium</option>
             <option value="large">Large</option>
             <option value="chunko">Chonker</option>
           </select>
-          <div>
           </div>
+           <div class="col-12 pt-4">
+           <div class="form-check">
+           <input class="form-check-input" type="checkbox" id="canFly"  name="canFly">
+             <label class="form-check-label" for="gridCheck">
+               Can it fly?
+            </label>
+           </div>
+          </div>
+          <div>
           <div class="modal-footer">
             <button type="button" class="btn bird-btn" data-bs-dismiss="modal">Close</button>
             <button type="submit" class="btn bird-btn-green">Submit</button>
@@ -84,4 +89,6 @@ export class Bird {
         </form>
         `
     }
+
+
 }
