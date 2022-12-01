65f2d6fa71731dae1c1f64b8bc44ae688603b96f
Jeremy Fowler <jembyfowler@gmail.com>
2022-12-01 15:34:27 -0700

    added account edit

--- server/controllers/AccountController.js
+++ server/controllers/AccountController.js
@@ -3,17 +3,26 @@ import { accountService } from '../services/AccountService'
 import BaseController from '../utils/BaseController'
 
 export class AccountController extends BaseController {
-  constructor() {
+  constructor () {
     super('account')
     this.router
       .use(Auth0Provider.getAuthorizedUserInfo)
       .get('', this.getUserAccount)
+      .put('', this.updateAccount)
   }
 
   async getUserAccount(req, res, next) {
     try {
       const account = await accountService.getAccount(req.userInfo)
       res.send(account)
+    } catch (error) {
+      next(error)
+    }
+  }
+  async updateAccount(req, res, next) {
+    try {
+      const account = await accountService.updateAccount(req.userInfo, req.body)
+      return res.send(account)
     } catch (error) {
       next(error)
     }
